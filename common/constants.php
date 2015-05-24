25d7ff535788aaa6ebc992d3f4443b619dbc2072
高松幸平 <gaoxin.xixxix@gmail.com>
2015-05-25 02:48:40 +0900

    ad追加

--- common/constants.php
+++ common/constants.php
@@ -2,5 +2,5 @@
 define("API_KEY","J7PJAlie5VNncUbJ0OVOjDFEE");
 define("API_SECRET","YGdJx0r8yViMng59jGhyWmvWVchkNI9hztz1RqhMj0vlATYm6l");
 define("TWEET_LIMIT",50);
-
+define("AD_1",'<!-- Rakuten Widget FROM HERE --><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="13ffb7a2.33ac82b6.13ffb7a3.0802bc0a";rakuten_items="ctsmatch";rakuten_genreId=0;rakuten_size="336x280";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="off";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";</script><script type="text/javascript" src="http://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script><!-- Rakuten Widget TO HERE -->');
 ?>
--- view/index_v.php
+++ view/index_v.php
@@ -48,26 +48,7 @@
                     </div>
                 </div>
                 <div class="adbox">
-                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- ad_2 -->
-                    <ins class="adsbygoogle"
-                         style="display:inline-block;width:336px;height:280px"
-                         data-ad-client="ca-pub-4596465970015112"
-                         data-ad-slot="3554412589"></ins>
-                    <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>
-                <div class="adbox">
-                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- ad_1 -->
-                    <ins class="adsbygoogle"
-                         style="display:inline-block;width:300px;height:600px"
-                         data-ad-client="ca-pub-4596465970015112"
-                         data-ad-slot="2496481784"></ins>
-                    <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
+                <?=AD_1?>
                 </div>
             </div>
         </div>
